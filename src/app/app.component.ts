bae77abfd460b75274394c377b425ba9133854d2
Trenton D. Adams <trenton.d.adams@gmail.com>
2017-08-31 20:31:26 -0600

    Tutorial 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,40 @@
 import {Component} from '@angular/core';
 
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-/*  template: `
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>`,*/
+  /*  template: `
+   <h1>{{title}}</h1>
+   <h2>{{hero.name}} details!</h2>
+   <div><label>id: </label>{{hero.id}}</div>
+   <div>
+   <label>name: </label>
+   <input [(ngModel)]="hero.name" placeholder="name">
+   </div>`,*/
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent
 {
   title = 'Tour of Heroes';
-  hero: Hero = {id: 1, name: 'Winstorm'}
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero
